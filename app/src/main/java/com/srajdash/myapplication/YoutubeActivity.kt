071b45c80c4f6a3722d36caf3546087c02ebfa44
Swaraj Dash <swarajdash40@gmail.com>
2020-10-05 12:40:32 +0530

    Add dynamic activity

--- app/src/main/java/com/srajdash/myapplication/YoutubeActivity.kt
+++ app/src/main/java/com/srajdash/myapplication/YoutubeActivity.kt
@@ -1,11 +1,35 @@
 package com.srajdash.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 
-class YoutubeActivity : AppCompatActivity() {
+class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
+        val layout = findViewById<ConstraintLayout>(R.id.activity_youtube)
+        val button1 = Button(this)
+        button1.layoutParams = ConstraintLayout.LayoutParams(600,180)
+        button1.text = "Button added"
+        layout.addView((button1))
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        TODO("Not yet implemented")
     }
 }
