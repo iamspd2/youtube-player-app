a352bb25f642d032578e62e3be141c474d608dba
Swaraj Dash <swarajdash40@gmail.com>
2020-10-05 12:21:25 +0530

    Add launcher activity

---
+++ app/src/main/java/com/srajdash/myapplication/YoutubeActivity.kt
@@ -0,0 +1,11 @@
+package com.srajdash.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YoutubeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+    }
+}
